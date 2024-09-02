b425898d986d58b944fe23122152fb04df0bf4b6
mohamedkamelx <167813862+mohamedkamelx@users.noreply.github.com>
2024-09-02 22:01:33 +0300

    encoder v1

---
+++ encoder.c
@@ -0,0 +1,27 @@
+/*
+ * File:   decoder.c
+ * Author: MKamel
+ *
+ * Created on September 2, 2024, 8:55 PM
+ */
+
+
+#include <xc.h>
+#include "encoder.h"
+
+volatile uint16_t pulse_count = 0;
+uint32_t previous_time = 0;
+uint32_t current_time = 0;
+float rpm = 0.0;
+
+
+
+void encoder_init(void) {
+    DDRD &= ~(1 << PD2);  //encoder signal on PD2 (INT0)
+    EICRA = (1 << ISC01); // Trigger interrupt on falling edga
+    EIMSK = (1 << INT0);  // Enable INT0
+}
+
+ISR(INT0_vect) {
+    pulse_count++;
+}
