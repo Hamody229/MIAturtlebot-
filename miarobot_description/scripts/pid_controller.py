89ff0d8b5ed055814ae58c279329fbb10263553b
Hamody229 <ahmedeslam229200@gmail.com>
2024-09-10 23:12:08 +0300

    Add files via upload

---
+++ miarobot_description/scripts/pid_controller.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+import rospy
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+import math
+
+class PIDController:
+    def __init__(self):
+        rospy.init_node('turtlebot_pid_controller', anonymous=True)
+        
+        # Publisher for velocity commands
+        self.vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        
+        # Subscriber for odometry (to get current position)
+        rospy.Subscriber('/odom', Odometry, self.odom_callback)
+
+        # PID gains
+        self.kp_linear = 1.0
+        self.kp_angular = 4.0
+        
+        # Robot's current position and orientation
+        self.x = 0.0
+        self.y = 0.0
+        self.theta = 0.0
+        
+        # Target position and orientation (change this to your desired goal)
+        self.x_target = 0.0 
+        self.y_target = 0.0  
+        self.theta_target = 0.0  
+        self.rate = rospy.Rate(10)
+
+    def odom_callback(self, data):
+        self.x = data.pose.pose.position.x
+        self.y = data.pose.pose.position.y
+        
+        orientation_q = data.pose.pose.orientation
+        (_, _, self.theta) = self.quaternion_to_euler(orientation_q)
+
+    def quaternion_to_euler(self, orientation_q):
+        import tf
+        return tf.transformations.euler_from_quaternion(
+            [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w])
+
+    def normalize_angle(self, angle):
+        while angle > math.pi:
+            angle -= 2.0 * math.pi
+        while angle < -math.pi:
+            angle += 2.0 * math.pi
+        return angle
+
+    def move_to_goal(self):
+        while not rospy.is_shutdown():
+            error_x = self.x_target - self.x
+            error_y = self.y_target - self.y
+            
+            distance = math.sqrt(error_x**2 + error_y**2)
+            angle_to_target = math.atan2(error_y, error_x)
+            
+            angular_error = self.normalize_angle(angle_to_target - self.theta)
+            linear_velocity = self.kp_linear * distance
+            if distance < 0.1:  # Stop when close to the target
+                linear_velocity = 0.0
+
+            angular_velocity = self.kp_angular * angular_error
+            if abs(angular_error) < 0.05: 
+                angular_velocity = 0.0
+
+            vel_cmd = Twist()
+            vel_cmd.linear.x = linear_velocity
+            vel_cmd.angular.z = angular_velocity
+            self.vel_pub.publish(vel_cmd)
+            
+            self.rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        controller = PIDController()
+        controller.move_to_goal()
+    except rospy.ROSInterruptException:
+        pass
